69950a1244a7a7435ecdddbe5165d6068ad399c6
Michael Smith <mikesmithnova@gmail.com>
2017-12-09 05:12:54 -0400

    Module 4 HW complete

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var slideInterval = 3500;
+function getFigures() {
+    return document.getElementById('carousel').getElementsByTagName('figure')
+}
+function moveForward() {
+    var figures = getFigures();
+    var numFigs = figures.length;
+    for (var i = 0; i < numFigs; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+            figures[++i % numFigs].className = 'visible';
+            break;
+        }
+    }
+    setTimeout(moveForward, slideInterval);
+}
+function startPlayback() {
+    setTimeout(moveForward, slideInterval);
+}
+startPlayback();
